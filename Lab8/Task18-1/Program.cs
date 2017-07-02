b0f1568c6bc24dd9c03d89426b48272f9e677abc
TAlekseeva1989 <tatyana_alekseeva@ro.ru>
2017-07-02 12:35:03 +0600

    Добавлены варианты решения задач 18-1 и 19-1 из лабораторной работы 8.

---
+++ Lab8/Task18-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_18_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t = Console.ReadLine();
+            string[] sarr = t.Split(' ');
+            int i = 0;
+            int L = sarr.Length;
+            int[] arr = new int[L];
+            while (i < L)
+            {
+                arr[i] = int.Parse(sarr[i]);
+                i++;
+            }
+            i = 0;
+            int C = arr[0];
+            while (i < L)
+            {
+                if (arr[i] < C)
+                {
+                    C = arr[i];
+                }
+                i++;
+            }
+            Console.WriteLine(C);
+        }
+    }
+}
